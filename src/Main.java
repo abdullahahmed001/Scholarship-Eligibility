2234c246ba801df229e0724461bbb9605ae4306a
Abdullah Ahmed <121642813+abdullahahmed001@users.noreply.github.com>
2026-02-21 23:42:43 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = myScanner.nextInt();
+
+        if(age < 18 && age > 24)
+        {
+            System.out.println("You are not eligible for a scholarship");
+        }
+        else
+        {
+            System.out.println("You are eligible for a scholarship");
+        }
+
+    }
+}
